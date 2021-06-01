c8fdff676949728a55ba4b1b48e10e0b11eecd03
hannez11 <49365734+hannez11@users.noreply.github.com>
2021-06-01 16:32:56 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ SESSION_CONFIGS = [
     dict(
        name='chat1',
        display_name="Chat",
-       num_demo_participants=1000,
+       num_demo_participants=3,
        app_sequence=['chat1']
     ),
 ]
